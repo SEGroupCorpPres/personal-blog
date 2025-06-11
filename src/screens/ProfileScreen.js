530e3fab9ba75ecb5248e4d9f6d6b3f156de0b34
Sulaymon O'rinov <artessdu@gmail.com>
2025-06-11 16:03:18 +0500

    profile screen yaratdim, reactstrap package olib tashladim

---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProfileScreen(props) {
+    return (
+        <div>
+            <h1>Hello Starscream</h1>
+        </div>
+    );
+}
+
+export default ProfileScreen;
