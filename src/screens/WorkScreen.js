2e581bfc7610224e5d6e87b101d9feae35ea66a2
mumtozbek-sadullayev@mumtozbek-sadullayev-Lenovo-V15-G4-IRU <sadullayevmumtozbek@gmail.com>
2025-06-13 16:02:52 +0500

    Man ina Work Screen va Portfolio Screeni  qismini yaratdim

--- src/screens/WorkScreen.js
+++ src/screens/WorkScreen.js
@@ -3,7 +3,7 @@ import React from 'react';
 function WorkScreen(props) {
     return (
         <div>
-            <h1>Work</h1>
+            <h1> this is search scrin woork</h1>
         </div>
     );
 }
